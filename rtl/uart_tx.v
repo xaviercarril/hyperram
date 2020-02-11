3e9e07e541eb6d200fc1b7074828c79562e7773b
Guillem <cabo.guillem@gmail.com>
2020-02-11 11:41:46 +0100

    code style

--- rtl/uart_tx.v
+++ rtl/uart_tx.v
@@ -50,64 +50,6 @@ wire load;    //-- Carga del registro de desplazamiento. Puesta a 0 del
               //-- contador de bits
 wire baud_en; //-- Habilitar el generador de baudios para la transmision
 
-//-------------------------------------
-//-- RUTA DE DATOS
-//-------------------------------------
-
-//-- Registrar la entrada start
-//-- (para cumplir con las reglas de diseño sincrono)
-always @(posedge clk)
-  start_r <= start;
-
-always @(posedge clk)
-  if (start == 1 && state == IDLE)
-    data_r <= data;
-
-//-- Registro de 10 bits para almacenar la trama a enviar:
-//-- 1 bit start + 8 bits datos + 1 bit stop
-reg [9:0] shifter;
-
-//-- Cuando la microorden load es 1 se carga la trama
-//-- con load 0 se desplaza a la derecha y se envia un bit, al
-//-- activarse la señal de clk_baud que marca el tiempo de bit
-//-- Se introducen 1s por la izquierda
-always @(posedge clk)
-  //-- Reset
-  if (rstn == 0)
-    shifter <= 10'b11_1111_1111;
-
-  //-- Modo carga
-  else if (load == 1)
-    shifter <= {data_r,2'b01};
-
-  //-- Modo desplazamiento
-  else if (load == 0 && clk_baud == 1)
-    shifter <= {1'b1, shifter[9:1]};
-
-//-- Contador de bits enviados
-//-- Con la microorden load (=1) se hace un reset del contador
-//-- con load = 0 se realiza la cuenta de los bits, al activarse
-//-- clk_baud, que indica el tiempo de bit
-always @(posedge clk)
-  if (load == 1)
-    bitc <= 0;
-  else if (load == 0 && clk_baud == 1)
-    bitc <= bitc + 1;
-
-//-- Sacar por tx el bit menos significativo del registros de desplazamiento
-//-- ES UNA SALIDA REGISTRADA, puesto que tx se conecta a un bus sincrono
-//-- y hay que evitar que salgan pulsos espureos (glitches)
-always @(posedge clk)
-  tx <= shifter[0];
-
-//-- Divisor para obtener el reloj de transmision
-baudgen #(.BAUD(BAUD))
-  BAUD0 (
-    .clk(clk),
-    .clk_ena(baud_en),
-    .clk_out(clk_baud)
-  );
-
 //------------------------------
 //-- CONTROLADOR
 //------------------------------
@@ -166,6 +108,64 @@ assign baud_en = (state == IDLE) ? 0 : 1;
 //-- Señal de salida. Esta a 1 cuando estamos en reposo (listos
 //-- para transmitir). En caso contrario esta a 0
 assign ready = (state == IDLE) ? 1 : 0;
+//-------------------------------------
+//-- RUTA DE DATOS
+//-------------------------------------
+
+//-- Registrar la entrada start
+//-- (para cumplir con las reglas de diseño sincrono)
+always @(posedge clk)
+  start_r <= start;
+
+always @(posedge clk)
+  if (start == 1 && state == IDLE)
+    data_r <= data;
+
+//-- Registro de 10 bits para almacenar la trama a enviar:
+//-- 1 bit start + 8 bits datos + 1 bit stop
+reg [9:0] shifter;
+
+//-- Cuando la microorden load es 1 se carga la trama
+//-- con load 0 se desplaza a la derecha y se envia un bit, al
+//-- activarse la señal de clk_baud que marca el tiempo de bit
+//-- Se introducen 1s por la izquierda
+always @(posedge clk)
+  //-- Reset
+  if (rstn == 0)
+    shifter <= 10'b11_1111_1111;
+
+  //-- Modo carga
+  else if (load == 1)
+    shifter <= {data_r,2'b01};
+
+  //-- Modo desplazamiento
+  else if (load == 0 && clk_baud == 1)
+    shifter <= {1'b1, shifter[9:1]};
+
+//-- Contador de bits enviados
+//-- Con la microorden load (=1) se hace un reset del contador
+//-- con load = 0 se realiza la cuenta de los bits, al activarse
+//-- clk_baud, que indica el tiempo de bit
+always @(posedge clk)
+  if (load == 1)
+    bitc <= 0;
+  else if (load == 0 && clk_baud == 1)
+    bitc <= bitc + 1;
+
+//-- Sacar por tx el bit menos significativo del registros de desplazamiento
+//-- ES UNA SALIDA REGISTRADA, puesto que tx se conecta a un bus sincrono
+//-- y hay que evitar que salgan pulsos espureos (glitches)
+always @(posedge clk)
+  tx <= shifter[0];
+
+//-- Divisor para obtener el reloj de transmision
+baudgen #(.BAUD(BAUD))
+  BAUD0 (
+    .clk(clk),
+    .clk_ena(baud_en),
+    .clk_out(clk_baud)
+  );
+
 
 endmodule
 
